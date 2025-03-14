3c6b19dfaa4c9aa56068a70708da1e24c9247d85
hll0017 <hll0017@auburn.edu>
2025-03-14 15:40:57 -0500

    Made script to make SRR ID list and Table
    Made a script to compile SRR IDs and Breed Sizes into a list and a table for use in subsequent scripts

---
+++ RNAseq_Samples/list_prep.sh
@@ -0,0 +1,20 @@
+# !/bin/bash
+
+# Goal: Make list of SRR IDs for the bioinformatic pipeline
+# Goal: Make a table of SRR IDs, Breed Names, and Breed Size
+
+# Extract Big Breeds' SRR IDs  and add to SRR_IDs.txt (exclude individuals marked with *)
+awk -F'\t' ' NR > 1 && $0 !~ /\*/ { print $1 }' Big_Breeds_List.txt > SRR_IDs.txt
+
+# Extract Small Breeds' IDs and append to SRR_IDs.txt (exclude individuals marked with *)
+awk -F'\t' ' NR > 1 && $0 !~ /\*/ { print $1 }' Small_Breeds_List.txt >> SRR_IDs.txt
+
+# Make table with SRR IDs, Breed Names, and Breed Sizes
+# Print header
+printf "SRR\tSize\tName\n" > Breed_Table.tsv
+
+# Append Big Dog Breeds to Breed_Table.tsv
+awk -F'\t' ' NR > 1 && $0 !~ /\*/ { print $1, FS, $2, FS, "Big" }' Big_Breeds_List.txt >> Breed_Table.tsv
+
+# Append Small Dog Breeds to Breed_Table.tsv
+awk -F'\t' ' NR > 1 && $0 !~ /\*/ { print $1, FS, $2, FS, "Small" }' Small_Breeds_List.txt >> Breed_Table.tsv
