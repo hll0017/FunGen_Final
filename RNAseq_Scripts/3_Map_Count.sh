b60150445a94fbfa24c91ad9152797ac0fc7387e
hll0017 <hll0017@auburn.edu>
2025-03-22 17:32:34 -0500

    Update 3_Map_Count.sh
    Modified for use for our samples (may be issue with copying data from class_shared)

--- RNAseq_Scripts/3_Map_Count.sh
+++ RNAseq_Scripts/3_Map_Count.sh
@@ -50,7 +50,7 @@ set -x
 ## Replace the numbers in the brackets with Your specific information
   ## make variable for your ASC ID so the directories are automatically made in YOUR directory
   ## Replace the [#] with paths to define these variable
-MyID=aubtss          ## Example: MyID=aubtss
+MyID=aubclsd0322          ## Example: MyID=aubtss
 
 WD=/scratch/$MyID/PracticeRNAseq            ## Example:/scratch/$MyID/PracticeRNAseq  
 CD=/scratch/$MyID/PracticeRNAseq/CleanData            ## Example:/scratch/$MyID/PracticeRNAseq/CleanData   #   *** This is where the cleaned paired files are located from the last script
@@ -59,7 +59,7 @@ MAPD=/scratch/$MyID/PracticeRNAseq/Map_HiSat2           ## Example:/scratch/$MyI
 COUNTSD=/scratch/$MyID/PracticeRNAseq/Counts_StringTie       ## Example:/scratch/$MyID/PracticeRNAseq/Counts_StringTie
 RESULTSD=/home/$MyID/PracticeRNAseq/Counts_H_S_2024      ## Example:/home/aubtss/PracticeRNAseq/Counts_H_S
 
-REF=DaphniaPulex_RefGenome_PA42_v3.0                  ## This is what the "easy name" will be for the genome reference
+REF=GCF_000002285.5_Dog10K_Boxer_Tasha_genomic                  ## This is what the "easy name" will be for the genome reference
 
 ## Make the directories and all subdirectories defined by the variables above
 mkdir -p $REFD
@@ -69,63 +69,60 @@ mkdir -p $RESULTSD
 
 ##################  Prepare the Reference Index for mapping with HiSat2   #############################
 cd $REFD
+
 ### Copy the reference genome (.fasta) and the annotation file (.gff3) to this REFD directory
-cp /home/${MyID}/class_shared/references/DaphniaPulex/PA42/${REF}.fasta .
-cp /home/${MyID}/class_shared/references/DaphniaPulex/PA42/${REF}.gff3 .
+cp /home/$MyID/class_shared/Dog_Tasha_GCF_000002285.5/data/GCF_000002285.5/$REF.fna .
+cp /home/$MyID/class_shared/Dog_Tasha_GCF_000002285.5/data/GCF_000002285.5/genomic.gff .
 
 ###  Identify exons and splice sites on the reference genome
-gffread ${REF}.gff3 -T -o ${REF}.gtf               ## gffread converts the annotation file from .gff3 to .gft formate for HiSat2 to use.
+gffread genomic.gff -T -o ${REF}.gtf               ## gffread converts the annotation file from .gff3 to .gft formate for HiSat2 to use.
 hisat2_extract_splice_sites.py ${REF}.gtf > ${REF}.ss
 hisat2_extract_exons.py ${REF}.gtf > ${REF}.exon
 
 #### Create a HISAT2 index for the reference genome. NOTE every mapping program will need to build a its own index.
-hisat2-build --ss ${REF}.ss --exon ${REF}.exon ${REF}.fasta DpulPA42_index
+hisat2-build --ss ${REF}.ss --exon ${REF}.exon ${REF}.fna Dog10K_Boxer_Tasha_index
 
 ########################  Map and Count the Data using HiSAT2 and StringTie  ########################
-
-# Move to the data directory
-cd ${CD}  #### This is where our clean paired reads are located.
-
-## Create list of fastq files to map.    Example file format of your cleaned reads file names: SRR629651_1_paired.fastq SRR629651_2_paired.fastq
-## grab all fastq files, cut on the underscore, use only the first of the cuts, sort, use unique put in list
-ls | grep ".fastq" |cut -d "_" -f 1| sort | uniq > list    #should list Example: SRR629651
 
 ## Move to the directory for mapping
 cd ${MAPD}
 
-## move the list of unique ids from the original files to map
-mv ${CD}/list  . 
+## Copy SRR_IDs.txt to Mapping Directory
+cp /home/$MyID/FunGen_Final/RNAseq_Samples/SRR_IDs.txt .
+
+## Create list of fastq files to map.
+SRR_IDs=$(cat SRR_IDs.txt)
 
 ## process the samples in the list, one by one using a while loop
-while read i;
+for SRR in ${SRR_IDs[@]}
 do
   ## HiSat2 is the mapping program
   ##  -p indicates number of processors, --dta reports alignments for StringTie --rf is the read orientation
-   hisat2 -p 6 --dta --phred33       \
-    -x "${REFD}"/DpulPA42_index       \
-    -1 "${CD}"/"$i"_1_paired.fastq  -2 "${CD}"/"$i"_2_paired.fastq      \
-    -S "$i".sam
+   hisat2 -p 12 --dta --phred33       \
+    -x "${REFD}"/Dog10K_Boxer_Tasha_index       \
+    -1 "${CD}"/"$SRR".fastq \
+    -S "$SRR".sam
 
     ### view: convert the SAM file into a BAM file  -bS: BAM is the binary format corresponding to the SAM text format.
     ### sort: convert the BAM file to a sorted BAM file.
     ### Example Input: SRR629651.sam; Output: SRR629651_sorted.bam
-  samtools view -@ 6 -bS "$i".sam > "$i".bam  
+  samtools view -@ 6 -bS "$SRR".sam > "$SRR".bam
 
     ###  This is sorting the bam, using 6 threads, and producing a .bam file that includes the word 'sorted' in the name
-  samtools sort -@ 6  "$i".bam  -o  "$i"_sorted.bam
+  samtools sort -@ 6  "$SRR".bam  -o  "$SRR"_sorted.bam
 
     ### Index the BAM and get mapping statistics, and put them in a text file for us to look at.
-  samtools flagstat   "$i"_sorted.bam   > "$i"_Stats.txt
+  samtools flagstat "$SRR"_sorted.bam   > "$SRR"_Stats.txt
 
   ### Stringtie is the program that counts the reads that are mapped to each gene, exon, transcript model. 
   ### The output from StringTie are counts folders in a directory that is ready to bring into the R program Ballgown to 
   ### Original: This will make transcripts using the reference geneome as a guide for each sorted.bam
   ### eAB options: This will run stringtie once and  ONLY use the Ref annotation for counting readsto genes and exons 
-  
-mkdir "${COUNTSD}"/"$i"
-stringtie -p 6 -e -B -G  "${REFD}"/"${REF}".gtf -o "${COUNTSD}"/"$i"/"$i".gtf -l "$i"   "${MAPD}"/"$i"_sorted.bam
 
-done<list
+	mkdir "${COUNTSD}"/"$SRR"
+	stringtie -p 12 -e -B -G  "${REFD}"/"${REF}".gtf -o "${COUNTSD}"/"$SRR"/"$SRR".gtf -l "$SRR"   "${MAPD}"/"$SRR"_sorted.bam
+
+done
 
 #####################  Copy Results to home Directory.  These will be the files you want to bring back to your computer.
 ### these are your stats files from Samtools
@@ -137,7 +134,7 @@ cd ${COUNTSD}
  ## run the python script prepDE.phy to prepare you data for downstream analysis.
 cp /home/${MyID}/class_shared/prepDE.py3 .
 
- prepDE.py3 -i ${COUNTSD}
+prepDE.py3 -i ${COUNTSD}
 
 ### copy the final results files (the count matricies that are .cvs) to your home directory. 
 cp *.csv ${RESULTSD}
